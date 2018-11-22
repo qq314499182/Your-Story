41eba5f3c8292e513b2363c2b32edd1fd895c4c3
Eddie Lee <931305033@qq.com>
2018-11-22 10:56:21 +0800

    config

--- src/main/java/com/warmth/yourstory/YourStoryApplication.java
+++ src/main/java/com/warmth/yourstory/YourStoryApplication.java
@@ -3,6 +3,11 @@ package com.warmth.yourstory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author eddie
+ * @createTime 2018-11-21
+ * @description 启动类
+ */
 @SpringBootApplication
 public class YourStoryApplication {
 
