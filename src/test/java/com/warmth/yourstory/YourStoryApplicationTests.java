5bb2e510943af7c5e8c1ab7e3a9ecc5f29efae0f
Eddie Lee <931305033@qq.com>
2018-11-22 10:52:43 +0800

    init commit

---
+++ src/test/java/com/warmth/yourstory/YourStoryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.warmth.yourstory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YourStoryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
